d17dcbaeb811ae15f0ec48654e826fe392e119d4
Mario Jordan <mjordanbartolo@gmail.com>
2021-09-11 20:52:51 -0500

    Change port declaration

--- my-app/server.js
+++ my-app/server.js
@@ -5,9 +5,8 @@ const http = require('http');
 const path = require('path');
 
 const appAngular = expressAngular();
-const port = 4200;
-console.log("port test", process.env.PORT);
+const port = 4200 || process.env.PORT;
 appAngular.use(expressAngular.static(__dirname + '/dist/my-app'));
 appAngular.get('/*', (req, res) => res.sendFile(path.join(__dirname)));
 const serverAngular = http.createServer(appAngular);
-serverAngular.listen(process.env.PORT || port, () => console.log(`App running on: http://localhost:${port}`));
+serverAngular.listen(port, () => console.log(`App running on: http://localhost:${port}`));
