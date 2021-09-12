bd03be0ae600f3f2f7aa78074cec1fe39ac2da1e
Mario Jordan <mjordanbartolo@gmail.com>
2021-09-11 20:45:33 -0500

    Ad dport logging

--- my-app/server.js
+++ my-app/server.js
@@ -6,6 +6,7 @@ const path = require('path');
 
 const appAngular = expressAngular();
 const port = 4200;
+console.log("port", process.env.PORT);
 appAngular.use(expressAngular.static(__dirname + '/dist/my-app'));
 appAngular.get('/*', (req, res) => res.sendFile(path.join(__dirname)));
 const serverAngular = http.createServer(appAngular);
