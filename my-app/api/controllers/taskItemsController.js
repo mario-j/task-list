41a60a6ef95326e2dede914b5de1a60ee8173b60
Mario Jordan <mjordanbartolo@gmail.com>
2021-09-11 19:18:57 -0500

    Remove unused api

--- my-app/api/controllers/taskItemsController.js
+++
@@ -1,8 +0,0 @@
-const csv = require('csv-parser');
-const fs = require('fs');
-const TaskItem = require('../models/taskItem');
-let items = [{id: 1, description: 'test'}];
-
-exports.get_items = function(req, res) {
-    res.send(items);
-};
--- my-app/api/routes/routes.js
+++
@@ -1,6 +0,0 @@
-'use strict';
-module.exports = function(app) {
-  var taskItems = require('../controllers/taskItemsController');
-
-  app.route('/api/get').get(taskItems.get_items)
-};
